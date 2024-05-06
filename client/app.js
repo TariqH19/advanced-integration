3cbfc75f2c06017c046b71450f45cbb3661aeb64
Tariq Horan <N00202437@iadt.ie>
2024-05-06 12:04:01 +0100

    Updated to follow the advanced integration doc

--- client/app.js
+++ client/app.js
@@ -1,4 +1,30 @@
-async function createOrderCallback() {
+function handleTransactionCases(orderData) {
+  const errorDetail = orderData?.details?.[0];
+  if (errorDetail?.issue === "INSTRUMENT_DECLINED") {
+    resultMessage(`<h3>Payment failed please try again</h3>`);
+    // (1) Recoverable INSTRUMENT_DECLINED -> call actions.restart()
+    // recoverable state, per https://developer.paypal.com/docs/checkout/standard/customize/handle-funding-failures/
+    return actions.restart();
+  } else if (errorDetail) {
+    // (2) Other non-recoverable errors -> Show a failure message
+    resultMessage(`<h3>Payment failed please try again</h3>`);
+    throw new Error(`${errorDetail.description} (${orderData.debug_id})`);
+  } else if (!orderData.purchase_units) {
+    throw new Error(JSON.stringify(orderData));
+  } else {
+    // (3) Successful transaction -> Show confirmation or thank you message
+    // Or go to another URL:  actions.redirect('thank_you.html');
+    const transaction = orderData.purchase_units[0].payments.captures[0];
+    resultMessage(`<h3>Transaction ${transaction.status}</h3>`);
+    console.log(
+      "Capture result",
+      orderData,
+      JSON.stringify(orderData, null, 2)
+    );
+  }
+}
+
+async function onCreateOrder() {
   try {
     const response = await fetch("/api/orders", {
       method: "POST",
@@ -10,8 +36,8 @@ async function createOrderCallback() {
       body: JSON.stringify({
         cart: [
           {
-            id: "YOUR_PRODUCT_ID",
-            quantity: "YOUR_PRODUCT_QUANTITY",
+            id: "Abbz7vGw_5c8_cdyGzrRM_ZmP8YISHGRbN0SeR1aWPF4XesBlhwds2M9bsMwHpeEaSyfqOrJKTvRuPkD",
+            quantity: "2",
           },
         ],
       }),
@@ -35,9 +61,9 @@ async function createOrderCallback() {
   }
 }
 
-async function onApproveCallback(data, actions) {
+async function onCaptureOrder(orderID) {
   try {
-    const response = await fetch(`/api/orders/${data.orderID}/capture`, {
+    const response = await fetch(`/api/orders/${orderID}/capture`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -45,100 +71,99 @@ async function onApproveCallback(data, actions) {
     });
 
     const orderData = await response.json();
-    // Three cases to handle:
-    //   (1) Recoverable INSTRUMENT_DECLINED -> call actions.restart()
-    //   (2) Other non-recoverable errors -> Show a failure message
-    //   (3) Successful transaction -> Show confirmation or thank you message
-
-    const transaction =
-      orderData?.purchase_units?.[0]?.payments?.captures?.[0] ||
-      orderData?.purchase_units?.[0]?.payments?.authorizations?.[0];
-    const errorDetail = orderData?.details?.[0];
-
-    // this actions.restart() behavior only applies to the Buttons component
-    if (errorDetail?.issue === "INSTRUMENT_DECLINED" && !data.card && actions) {
-      // (1) Recoverable INSTRUMENT_DECLINED -> call actions.restart()
-      // recoverable state, per https://developer.paypal.com/docs/checkout/standard/customize/handle-funding-failures/
-      return actions.restart();
-    } else if (
-      errorDetail ||
-      !transaction ||
-      transaction.status === "DECLINED"
-    ) {
-      // (2) Other non-recoverable errors -> Show a failure message
-      let errorMessage;
-      if (transaction) {
-        errorMessage = `Transaction ${transaction.status}: ${transaction.id}`;
-      } else if (errorDetail) {
-        errorMessage = `${errorDetail.description} (${orderData.debug_id})`;
-      } else {
-        errorMessage = JSON.stringify(orderData);
-      }
-
-      throw new Error(errorMessage);
-    } else {
-      // (3) Successful transaction -> Show confirmation or thank you message
-      // Or go to another URL:  actions.redirect('thank_you.html');
-      resultMessage(
-        `Transaction ${transaction.status}: ${transaction.id}<br><br>See console for all available details`
-      );
-      console.log(
-        "Capture result",
-        orderData,
-        JSON.stringify(orderData, null, 2)
-      );
-    }
+    handleTransactionCases(orderData);
   } catch (error) {
     console.error(error);
-    resultMessage(
-      `Sorry, your transaction could not be processed...<br><br>${error}`
-    );
   }
 }
 
-window.paypal
+/*
+Example function to show a result to the user. Your site's UI library can be used instead,
+however alert() should not be used as it will interrupt the JS SDK window
+*/
+
+function resultMessage(message) {
+  const container = document.getElementById("paypal-button-container");
+  const p = document.createElement("p");
+  p.innerHTML = message;
+  container.parentNode.appendChild(p);
+}
+
+paypal
   .Buttons({
-    createOrder: createOrderCallback,
-    onApprove: onApproveCallback,
+    style: {
+      shape: "rect",
+      // color:'blue' change the default color of the buttons
+      layout: "vertical", // default value. Can be changed to horizontal
+    },
+    createOrder: onCreateOrder,
+    onApprove: async (data) => onCaptureOrder(data.orderID),
   })
   .render("#paypal-button-container");
 
-const cardField = window.paypal.CardFields({
-  createOrder: createOrderCallback,
-  onApprove: onApproveCallback,
-});
+// If this returns false or the card fields aren't visible, see Step #1.
+if (paypal.HostedFields.isEligible()) {
+  let orderId;
 
-// Render each field after checking for eligibility
-if (cardField.isEligible()) {
-  const nameField = cardField.NameField();
-  nameField.render("#card-name-field-container");
+  // Renders card fields
+  paypal.HostedFields.render({
+    // Call your server to set up the transaction
+    createOrder: async (data, actions) => {
+      orderId = await onCreateOrder(data, actions);
+      return orderId;
+    },
+    styles: {
+      ".valid": {
+        color: "green",
+      },
+      ".invalid": {
+        color: "red",
+      },
+    },
+    fields: {
+      number: {
+        selector: "#card-number",
+        placeholder: "4111 1111 1111 1111",
+      },
+      cvv: {
+        selector: "#cvv",
+        placeholder: "123",
+      },
+      expirationDate: {
+        selector: "#expiration-date",
+        placeholder: "MM/YY",
+      },
+    },
+  }).then((cardFields) => {
+    document
+      .querySelector("#card-form")
+      .addEventListener("submit", async (event) => {
+        event.preventDefault();
+        try {
+          const { value: cardHolderName } =
+            document.getElementById("card-holder-name");
+          const { value: postalCode } = document.getElementById(
+            "card-billing-address-zip"
+          );
+          const { value: countryCodeAlpha2 } = document.getElementById(
+            "card-billing-address-country"
+          );
 
-  const numberField = cardField.NumberField();
-  numberField.render("#card-number-field-container");
+          await cardFields.submit({
+            cardHolderName,
+            billingAddress: {
+              postalCode,
+              countryCodeAlpha2,
+            },
+          });
 
-  const cvvField = cardField.CVVField();
-  cvvField.render("#card-cvv-field-container");
-
-  const expiryField = cardField.ExpiryField();
-  expiryField.render("#card-expiry-field-container");
-
-  // Add click listener to submit button and call the submit function on the CardField component
-  document
-    .getElementById("multi-card-field-button")
-    .addEventListener("click", () => {
-      cardField.submit().catch((error) => {
-        resultMessage(
-          `Sorry, your transaction could not be processed...<br><br>${error}`
-        );
+          await onCaptureOrder(orderId);
+        } catch (error) {
+          alert("Payment could not be captured! " + JSON.stringify(error));
+        }
       });
-    });
+  });
 } else {
   // Hides card fields if the merchant isn't eligible
   document.querySelector("#card-form").style = "display: none";
-}
-
-// Example function to show a result to the user. Your site's UI library can be used instead.
-function resultMessage(message) {
-  const container = document.querySelector("#result-message");
-  container.innerHTML = message;
 }
---
+++ server/paypal-api.js
@@ -0,0 +1,115 @@
+import fetch from "node-fetch";
+
+// set some important variables
+const { PAYPAL_CLIENT_ID, PAYPAL_CLIENT_SECRET } = process.env;
+const base = "https://api-m.sandbox.paypal.com";
+
+/**
+ * Create an order to start the transaction.
+ * @see https://developer.paypal.com/docs/api/orders/v2/#orders_create
+ */
+export async function createOrder() {
+  const purchaseAmount = "100.00";
+  const accessToken = await generateAccessToken();
+  const url = `${base}/v2/checkout/orders`;
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${accessToken}`,
+      // Uncomment one of these to force an error for negative testing (in sandbox mode only). Documentation:
+      // https://developer.paypal.com/tools/sandbox/negative-testing/request-headers/
+      // "PayPal-Mock-Response": '{"mock_application_codes": "MISSING_REQUIRED_PARAMETER"}'
+      // "PayPal-Mock-Response": '{"mock_application_codes": "PERMISSION_DENIED"}'
+      // "PayPal-Mock-Response": '{"mock_application_codes": "INTERNAL_SERVER_ERROR"}'
+    },
+    body: JSON.stringify({
+      intent: "CAPTURE",
+      purchase_units: [
+        {
+          amount: {
+            currency_code: "USD",
+            value: purchaseAmount,
+          },
+        },
+      ],
+    }),
+  });
+
+  return handleResponse(response);
+}
+
+/**
+ * Capture payment for the created order to complete the transaction.
+ * @see https://developer.paypal.com/docs/api/orders/v2/#orders_capture
+ */
+export async function capturePayment(orderId) {
+  const accessToken = await generateAccessToken();
+  const url = `${base}/v2/checkout/orders/${orderId}/capture`;
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${accessToken}`,
+      // Uncomment one of these to force an error for negative testing (in sandbox mode only). Documentation:
+      // https://developer.paypal.com/tools/sandbox/negative-testing/request-headers/
+      // "PayPal-Mock-Response": '{"mock_application_codes": "INSTRUMENT_DECLINED"}'
+      // "PayPal-Mock-Response": '{"mock_application_codes": "TRANSACTION_REFUSED"}'
+      // "PayPal-Mock-Response": '{"mock_application_codes": "INTERNAL_SERVER_ERROR"}'
+    },
+  });
+
+  return handleResponse(response);
+}
+
+/**
+ * Generate an OAuth 2.0 access token for authenticating with PayPal REST APIs.
+ * @see https://developer.paypal.com/api/rest/authentication/
+ */
+export async function generateAccessToken() {
+  try {
+    if (!PAYPAL_CLIENT_ID || !PAYPAL_CLIENT_SECRET) {
+      throw new Error("MISSING_API_CREDENTIALS");
+    }
+    const auth = Buffer.from(
+      PAYPAL_CLIENT_ID + ":" + PAYPAL_CLIENT_SECRET
+    ).toString("base64");
+    const response = await fetch(`${base}/v1/oauth2/token`, {
+      method: "POST",
+      body: "grant_type=client_credentials",
+      headers: {
+        Authorization: `Basic ${auth}`,
+      },
+    });
+
+    const data = await response.json();
+    return data.access_token;
+  } catch (error) {
+    console.error("Failed to generate Access Token:", error);
+  }
+}
+
+// generate client token
+export async function generateClientToken() {
+  const accessToken = await generateAccessToken();
+  const response = await fetch(`${base}/v1/identity/generate-token`, {
+    method: "post",
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+      "Accept-Language": "en_US",
+      "Content-Type": "application/json",
+    },
+  });
+  console.log("response", response.status);
+  const jsonData = await handleResponse(response);
+  return jsonData.client_token;
+}
+
+async function handleResponse(response) {
+  if (response.status === 200 || response.status === 201) {
+    return response.json();
+  }
+
+  const errorMessage = await response.text();
+  throw new Error(errorMessage);
+}
