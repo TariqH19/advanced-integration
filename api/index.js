3e4773123fcb6dd7cc9e35108075bf4e859ade87
TariqH19 <tariqhoran88@gmail.com>
2024-08-01 11:30:40 +0100

    api

--- api/index.js
+++ api/index.js
@@ -1,3 +1,79 @@
-const server = require("../server/server");
+import "dotenv/config";
+import express from "express";
+import { fileURLToPath } from "url";
+import path from "path";
+import {
+  getOrderDetails,
+  createOrder,
+  capturePayment,
+  generateAccessToken,
+} from "./paypal-api.js"; // Import your PayPal helper functions
 
-module.exports = server;
+// Convert file URL to file path
+const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+// Host static files
+const clientPath = path.join(__dirname, "../client");
+app.use(express.static(clientPath));
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+// Middleware to parse JSON requests
+app.use(express.json());
+
+// Render checkout page with client ID
+app.get("/", async (req, res) => {
+  const clientId = process.env.PAYPAL_CLIENT_ID;
+  const tokenId = await generateAccessToken();
+
+  res.render("checkout", {
+    clientId,
+    tokenId,
+  });
+});
+
+app.post("/api/orders", async (req, res) => {
+  try {
+    const { task, saveCard } = req.body;
+    const order = await createOrder(task, saveCard);
+    res.json(order);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to create order" });
+  }
+});
+
+app.get("/api/orders/:orderID", async (req, res) => {
+  const { orderID } = req.params; // Ensure orderID is defined here
+  try {
+    const orderDetails = await getOrderDetails(orderID);
+    res.json(orderDetails);
+  } catch (error) {
+    console.error(
+      `Error fetching order details for ${orderID}:`,
+      error.message
+    );
+    res
+      .status(500)
+      .json({ error: `Failed to get order details: ${error.message}` });
+  }
+});
+
+// Capture payment
+app.post("/api/orders/:orderID/capture", async (req, res) => {
+  try {
+    const { orderID } = req.params;
+    const captureData = await capturePayment(orderID);
+    res.json(captureData);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to capture payment" });
+  }
+});
+
+app.listen(8888, () => {
+  console.log("Listening on http://localhost:8888/");
+});
