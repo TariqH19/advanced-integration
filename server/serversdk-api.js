11895f471523abd866fd6177ec36323e39c4ff3a
Tariq Horan <tariqhoran88@gmail.com>
2025-07-06 17:38:34 +0100

    Update PayPal order creation to include return and cancel URLs, and modify shipping options callback URL

--- server/serversdk-api.js
+++ server/serversdk-api.js
@@ -134,13 +134,19 @@ export async function newCreateOrder(cart) {
         paypal: {
           experienceContext: {
             userAction: PaypalExperienceUserAction.PayNow,
-
+            returnUrl:
+              "https://developer.paypal.com/studio/checkout/standard/integrate?appswitch=true",
+            cancelUrl:
+              "https://developer.paypal.com/studio/checkout/standard/integrate?appswitch=true",
+            appSwitchPreference: {
+              launchPaypalApp: true,
+            },
             landingPage: PaypalExperienceLandingPage.Login,
             shippingPreference: ShippingPreference.GetFromFile,
             orderUpdateCallbackConfig: {
               callbackEvents: ["SHIPPING_ADDRESS", "SHIPPING_OPTIONS"],
               callbackUrl:
-                "https://advanced-integration.vercel.app/paypal/shipping-options",
+                "https://p4vvvahfm0.execute-api.us-east-2.amazonaws.com/default/ssc",
             },
             contactPreference: "UPDATE_CONTACT_INFO",
           },
@@ -151,7 +157,7 @@ export async function newCreateOrder(cart) {
   };
 
   try {
-    const { body, ...httpResponse } = await newOrdersController.createOrder(
+    const { body, ...httpResponse } = await ordersController.createOrder(
       collect
     );
     // Get more response info...
